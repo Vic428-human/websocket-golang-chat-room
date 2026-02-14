cf80df29a24b1240d958740d4bc17e3428cb4400
Vic428-human <z0983195379a@gmail.com>
2026-02-15 01:43:33 +0800

    加 WebSocket endpoint

---
+++ cmd/server/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 256,
+	// Demo 用：先允許所有 origin，正式環境要收緊
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("ok"))
+	})
+
+	mux.HandleFunc("/ws", func(w http.ResponseWriter, req *http.Request) {
+		conn, err := upgrader.Upgrade(w, req, nil)
+		if err != nil {
+			log.Println("upgrade:", err)
+			return
+		}
+		defer conn.Close()
+
+		for {
+			mt, msg, err := conn.ReadMessage()
+			if err != nil {
+				return
+			}
+			if err := conn.WriteMessage(mt, msg); err != nil {
+				return
+			}
+		}
+	})
+
+	addr := ":8080"
+	log.Printf("listening on %s", addr)
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Vic428-human/websocket-golang-chat-room
 
 go 1.25.5
+
+require github.com/gorilla/websocket v1.5.3 // indirect
