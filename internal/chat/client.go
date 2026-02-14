bccc6e1bc154a8db1a774e377ecad3eb90cd36f2
Vic428-human <z0983195379a@gmail.com>
2026-02-15 02:21:53 +0800

    實作 client read/write pumps（串 forward/receive）

--- internal/chat/client.go
+++ internal/chat/client.go
@@ -7,3 +7,33 @@ type Client struct {
 	receive chan []byte
 	room    *Room
 }
+
+func NewClient(conn *websocket.Conn, room *Room) *Client {
+	return &Client{
+		socket:  conn,
+		receive: make(chan []byte, 256),
+		room:    room,
+	}
+}
+
+func (c *Client) ReadPump() {
+	defer c.socket.Close()
+
+	for {
+		_, msg, err := c.socket.ReadMessage()
+		if err != nil {
+			return
+		}
+		c.room.forward <- msg
+	}
+}
+
+func (c *Client) WritePump() {
+	defer c.socket.Close()
+
+	for msg := range c.receive {
+		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
+			return
+		}
+	}
+}
